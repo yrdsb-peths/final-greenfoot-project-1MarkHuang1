5b0f0e5052df4aa665ac56b5a3eabe6f923703e3
1MarkHuang1 <350092060@gapps.yrdsb.ca>
2023-12-15 09:46:50 -0500

    Added animation for "s"
    Can't figure out a way to make the animation slower
    Prob figure this out later

--- Player.java
+++ Player.java
@@ -3,18 +3,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Player extends Actor
 {
-    
+    SimpleTimer animationTimer = new SimpleTimer();
+    GreenfootImage[] walk = new GreenfootImage[3];    
     public Player(){
-        GreenfootImage image = getImage();
-        image.scale(51,61);
-        setImage(image);
+        for  (int i = 0; i< walk.length; i++){
+            walk[i] = new GreenfootImage("images/char/char" + i + ".png");
+            walk[i].scale(51,61);
+        }
+        setImage(walk[0]);
     }
+    int imageIndex = 0;
     public void act()
     {
         if (Greenfoot.isKeyDown("a")){
             move(-5);
         }
-        if(Greenfoot.isKeyDown("d")){
+        if(Greenfoot.isKeyDown("d") ){
             move(5);
         }
         if(Greenfoot.isKeyDown("w")){
@@ -22,7 +26,11 @@ public class Player extends Actor
         }
         if(Greenfoot.isKeyDown("s")){
             setLocation(getX() ,getY()+5);
-
+            setImage(walk[imageIndex]);
+            imageIndex = (imageIndex + 1)% walk.length;
+        }
+        else{
+            setImage(walk[0]);
         }
     }
 }
