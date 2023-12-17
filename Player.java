81b63fa9998d7f246c4d10a91f7e9ce286702202
1MarkHuang1 <350092060@gapps.yrdsb.ca>
2023-12-16 20:41:58 -0500

    Finished animation for left and right
    Only animation for 'w' is left
    {Cant find ways to keep it facing front when no keys are pressed }

--- Player.java
+++ Player.java
@@ -5,10 +5,22 @@ public class Player extends Actor
 {
     SimpleTimer animationTimer = new SimpleTimer();
     GreenfootImage[] walk = new GreenfootImage[3];    
+    GreenfootImage[] right = new GreenfootImage[3];
+    GreenfootImage[] left = new GreenfootImage[3];
+
     public Player(){
         for  (int i = 0; i< walk.length; i++){
-            walk[i] = new GreenfootImage("images/char/char" + i + ".png");
-            walk[i].scale(51,61);
+            walk[i] = new GreenfootImage("images/front/char" + i + ".png");
+            walk[i].scale(25,30);
+        }
+        for (int i = 0; i < right.length; i++){
+            right[i] = new GreenfootImage("images/right/right" + i + ".png");
+            right[i].scale(25,30);
+        }
+        for (int i = 0; i < left.length; i++){
+            left[i] = new GreenfootImage("images/right/right" + i + ".png");
+            left[i].mirrorHorizontally();
+            left[i].scale(25,30);
         }
         setImage(walk[0]);
     }
@@ -16,21 +28,23 @@ public class Player extends Actor
     public void act()
     {
         if (Greenfoot.isKeyDown("a")){
-            move(-5);
+            move(-3);
+            setImage(left[imageIndex]);
+            imageIndex = (imageIndex + 1)% left.length;
         }
         if(Greenfoot.isKeyDown("d") ){
-            move(5);
+            move(3);
+            setImage(right[imageIndex]);
+            imageIndex = (imageIndex + 1)% right.length;
         }
         if(Greenfoot.isKeyDown("w")){
-            setLocation(getX() ,getY()-5);
+            setLocation(getX() ,getY()-3);
         }
         if(Greenfoot.isKeyDown("s")){
-            setLocation(getX() ,getY()+5);
+            setLocation(getX() ,getY()+3);
             setImage(walk[imageIndex]);
             imageIndex = (imageIndex + 1)% walk.length;
         }
-        else{
-            setImage(walk[0]);
-        }
+        
     }
 }
